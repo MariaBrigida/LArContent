69716944bcb578dd3ca40340f6a056596ea5609c
Andrew Chappell <A.Chappell@warwick.ac.uk>
2021-12-07 13:13:38 +0000

    Hack shower calorimetry checks

--- larpandoracontent/LArThreeDReco/LArPfoRecovery/ParticleConsistencyAlgorithm.cc
+++ larpandoracontent/LArThreeDReco/LArPfoRecovery/ParticleConsistencyAlgorithm.cc
@@ -47,6 +47,44 @@ float bethe_bloch(const float p, const float M)
     return dedx;
 }
 
+void get_longitudinal_energy_profile(const float e0, const FloatVector &positions, const float cj, const float binSize,
+    FloatVector &fractionalEnergies)
+{
+    const unsigned int N{static_cast<unsigned int>(positions.size())};
+    const float eCrit{32.84f};
+    const float b{0.5f};
+    float a{0.f};
+    if (e0 > eCrit)
+        a = (1 + b * cj) + b * std::log(e0 / eCrit);
+    else
+        a = 1 + b * cj;
+    float gammaA{std::tgamma(a)};
+    float cumulativeEnergy{0.f};
+    for (unsigned int i = 0; i < N; ++i)
+    {
+        const float t{positions[i]};
+        fractionalEnergies[i] = b * std::pow(b * t * binSize, a - 1) * std::exp(-b * t) / gammaA;
+        cumulativeEnergy += fractionalEnergies[i];
+    }
+    if (cumulativeEnergy > std::numeric_limits<float>::epsilon())
+    {
+        for (unsigned int i = 0; i < N; ++i)
+            fractionalEnergies[i] /= cumulativeEnergy;
+    }
+}
+
+void get_photon_longitudinal_energy_profile(const float e0, const FloatVector &positions, const float binSize,
+    FloatVector &fractionalEnergies)
+{
+    get_longitudinal_energy_profile(e0, positions, +0.5f, binSize, fractionalEnergies);
+}
+
+void get_electron_longitudinal_energy_profile(const float e0, const FloatVector &positions, const float binSize,
+    FloatVector &fractionalEnergies)
+{
+    get_longitudinal_energy_profile(e0, positions, -0.5f, binSize, fractionalEnergies);
+}
+
 ParticleConsistencyAlgorithm::ParticleConsistencyAlgorithm()
 {
 }
@@ -100,7 +138,7 @@ StatusCode ParticleConsistencyAlgorithm::Run()
                 tMin = tProj;
             if (tProj > tMax)
                 tMax = tProj;
-            tProjEnergyVec.emplace_back(std::make_pair(tProj, pCaloHit->GetInputEnergy()));
+            tProjEnergyVec.emplace_back(std::make_pair(tProj, 1000.f * pCaloHit->GetElectromagneticEnergy()));
         }
         // Determine which end of the longitudinal projection is closest to the vertex
         float start{0.f};
@@ -109,21 +147,21 @@ StatusCode ParticleConsistencyAlgorithm::Run()
         else
             start = lMax;
         // Adjust projection so that the extremal hit closest to the vertex is longitudinally projected onto zero
-        float totalEnergy{0.f};
         for (const CaloHit *const pCaloHit : caloHitList)
         {
             const CartesianVector &pos{pCaloHit->GetPositionVector()};
             const float lProj{std::abs(pos.GetDotProduct(lAxis) - start)};
             const float hitEnergy{pCaloHit->GetElectromagneticEnergy()};
-            lProjEnergyVec.emplace_back(std::make_pair(lProj, hitEnergy));
-            totalEnergy += hitEnergy;
+            lProjEnergyVec.emplace_back(std::make_pair(lProj, 1000.f * hitEnergy));
         }
-        // GeV to MeV
-        totalEnergy *= 1e3;
         const float lBinMax{std::max(std::abs(lMax - start), std::abs(lMin - start)) + std::numeric_limits<float>::epsilon()};
         const float radiationLength{14.f};
         const float lBinSize{0.5f * radiationLength};
         const int nBinsL{static_cast<int>(std::ceil(lBinMax / lBinSize))};
+
+        if (nBinsL <= 1)
+            continue;
+
         FloatVector binnedLongEnergy(nBinsL);
         std::cout << "N: " << nBinsL << std::endl;
         for (const auto point : lProjEnergyVec)
@@ -132,31 +170,63 @@ StatusCode ParticleConsistencyAlgorithm::Run()
             binnedLongEnergy[bin] += point.second;
         }
 
-        FloatVector binnedMuonEnergy(nBinsL);
-        FloatVector binnedPionEnergy(nBinsL);
-        FloatVector binnedProtonEnergy(nBinsL);
-        float residualEnergy{totalEnergy};
+        // Get theoretical shower profiles
+        const float binSize{0.5f};
+        FloatVector binCentres(nBinsL);
+        for (int i = 0; i < nBinsL; ++i)
+            binCentres[i] = 0.5f * (2 * i + 1) * binSize;
+        const float e0{std::accumulate(binnedLongEnergy.begin(), binnedLongEnergy.end(), 0.f)};
+
+//        for (int i = 0; i < nBinsL; ++i)
+//            binnedLongEnergy[i] /= e0;
+
+        FloatVector electronProfile(nBinsL), photonProfile(nBinsL);
+        get_electron_longitudinal_energy_profile(e0, binCentres, binSize, electronProfile);
+        get_photon_longitudinal_energy_profile(e0, binCentres, binSize, photonProfile);
+
         for (int i = 0; i < nBinsL; ++i)
         {
-            std::cout << "Residual: " << residualEnergy << std::endl;
-            if (residualEnergy > 0.f)
-            {
-                binnedMuonEnergy[i] = bethe_bloch(residualEnergy, 105.658f) * lBinSize;
-                binnedPionEnergy[i] = bethe_bloch(residualEnergy, 139.570f) * lBinSize;
-                binnedProtonEnergy[i] = bethe_bloch(residualEnergy, 938.272f) * lBinSize;
-            }
-            else
-            {
-                binnedMuonEnergy[i] = 0.f;
-                binnedPionEnergy[i] = 0.f;
-                binnedProtonEnergy[i] = 0.f;
-            }
-
-            residualEnergy -= (binnedLongEnergy[i] * 1e3);
-            std::cout << i << ": " << (binnedLongEnergy[i] * 1e3) << " dedx(mu): " << binnedMuonEnergy[i] <<
-                " dedx(pi): " << binnedPionEnergy[i] << " dedx(p): " << binnedProtonEnergy[i] << std::endl;
+            electronProfile[i] *= e0;
+            photonProfile[i] *= e0;
         }
-        std::cout << std::endl;
+
+        // Assess profiles
+        float photonChi2{0.f}, electronChi2{0.f};
+        // Only look at the first 6 bins
+        const int nShowerBins{std::min(nBinsL, 6)};
+        for (int i = 0; i < nShowerBins; ++i)
+        {
+            float dE{binnedLongEnergy[i] - electronProfile[i]};
+            electronChi2 += dE * dE / electronProfile[i];
+            dE = binnedLongEnergy[i] - photonProfile[i];
+            photonChi2 += dE * dE / photonProfile[i];
+
+            std::cout << i << " - Actual: " << binnedLongEnergy[i] << " " << electronProfile[i] << " " << photonProfile[i] << std::endl;
+        }
+        electronChi2 /= nShowerBins - 1;
+        photonChi2 /= nShowerBins - 1;
+
+        std::cout << "Chi2(e): " << electronChi2 << " Chi2(gamma): " << photonChi2 << std::endl;
+
+        // Look for approx constant energy deposition prior to a potential Bragg peak (essentially the last bin)
+        const int nConstantBins{nBinsL - 1};
+        FloatVector gradient(nConstantBins - 1);
+        int drop{0};
+        for (int i = 0; i < nConstantBins - 1; ++i)
+        {
+            const float scale{std::max(std::abs(binnedLongEnergy[i + 1]), std::abs(binnedLongEnergy[i]))};
+            if (scale < std::numeric_limits<float>::epsilon())
+            {
+                // Both bins are zero, drop the bin from the calculation
+                ++drop;
+                continue;
+            }
+            gradient[i] = (binnedLongEnergy[i + 1] - binnedLongEnergy[i]) / (scale * binSize);
+            gradient[i] *= gradient[i];
+            std::cout << i << " - Grad: " << gradient[i] << std::endl;
+        }
+        const float flatness{drop < (nConstantBins - 1) ? std::sqrt(std::accumulate(gradient.begin(), gradient.end(), 0.f) / (nConstantBins - (drop + 1))) : 0.f};
+        std::cout << "Flatness: " << flatness << std::endl;
 
         // Sum up energy, get theory profiles for e and gamma (and tracks too)
         // Compare, also generally assess the shape etc to look for problems
