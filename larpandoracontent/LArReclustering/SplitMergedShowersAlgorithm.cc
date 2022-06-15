300514b8b7f7e11beca7b4f8025f3e18ab1d3885
Maria Brunetti <phsjrg@scanuppia.epp.warwick.ac.uk>
2022-06-15 10:07:21 +0100

    Add skeleton for SplitMergedShowers reclustering algorithm

---
+++ larpandoracontent/LArReclustering/SplitMergedShowersAlgorithm.cc
@@ -0,0 +1,24 @@
+/**
+ *  @file   larpandoracontent/LArReclustering/SplitMergedShowersAlgorithm.cc
+ *
+ *  @brief  Implementation file for the reclustering algorithm that runs other algs.
+ *
+ *  $Log: $
+ */
+
+#include "Pandora/AlgorithmHeaders.h"
+
+#include "larpandoracontent/LArReclustering/SplitMergedShowersAlgorithm.cc"
+
+using namespace pandora;
+
+namespace lar_content
+{
+
+StatusCode SplitMergedShowersAlgorithm::Run()
+{
+
+    return STATUS_CODE_SUCCESS;
+}
+
+} // namespace lar_content
---
+++ larpandoracontent/LArReclustering/SplitMergedShowersAlgorithm.h
@@ -0,0 +1,19 @@
+/**
+ *  @file   larpandoracontent/LArReclustering/SplitMergedShowersAlgorithm.h
+ *
+ *  @brief  Header file for the reclustering algorithm that runs other algs.
+ *
+ *  $Log: $
+ */
+
+#ifndef LAR_SPLIT_MERGED_SHOWERS_ALGORITHM_H
+#define LAR_SPLIT_MERGED_SHOWERS_ALGORITHM_H 1
+
+#include "Pandora/Algorithm.h"
+
+namespace lar_content
+{
+
+} // namespace lar_content
+
+#endif // #ifndef LAR_SPLIT_MERGED_SHOWERS_ALGORITHM_H
